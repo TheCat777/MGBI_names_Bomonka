ce0a5a627fb21b93b557fda238d082bfb3087d9e
dranniks <136456377+dranniks@users.noreply.github.com>
2024-03-22 18:43:04 +0300

    cat fonk

--- Game/MiniGame.h
+++ Game/MiniGame.h
@@ -15,30 +15,53 @@
 
 class MiniGame : public Base_Scene{
 private:
-    double SPEED = 1;
+    std::string path_to_game = std::filesystem::current_path().parent_path().parent_path().string();
+    sf::Sound sound;
+    sf::SoundBuffer buffer;
+    double SPEED_W, SPEED_A, SPEED_S, SPEED_D;
+    int dw, da, ds, dd;
 public:
-    explicit MiniGame(sf::RenderWindow & window) {
+    MiniGame(sf::RenderWindow & window) {
         Load();
+        LoadMusic();
         while (window.isOpen()) {
             EventsMiniGame(window);
             DrawMiniGame(window);
         }
     }
-
+    void LoadMusic() {
+        if (!buffer.loadFromFile(path_to_game + "\\resources\\fonk.mp3")) {std::cout << "ERROR MUSIC"; }
+        sound.setBuffer(buffer);
+        sound.setVolume(10.f);
+        sound.setLoop(true);
+        sound.play();
+    }
     void DrawMiniGame(sf::RenderWindow & window){
         window.clear();
         for (auto& texture : Textures)
             texture.draw(window);
         window.display();
     }
+    void Collision(Texture txr) {
+        if (Textures[0].GetSprite().getGlobalBounds().intersects(txr.GetSprite().getGlobalBounds())) {
+            if (dw) {Textures[0].move(0, SPEED_S); SPEED_W = 0;}
+            if (da) {Textures[0].move(SPEED_D, 0); SPEED_A = 0;}
+            if (ds) {Textures[0].move(0, -SPEED_W); SPEED_S = 0;}
+            if (dd) {Textures[0].move(-SPEED_A, 0); SPEED_D = 0;}
+        }
+    }
     void Load() {
         Texture student;
         Texture wall;
-        student.create("\\resources\\student.jpg", {150, 500});
+        Texture wall2;
+
+        student.create("\\resources\\student.jpg", {100, 600});
         wall.create("\\resources\\wall.png", {100, 100});
+        wall2.create("\\resources\\wall.png", {400, 800});
 
         Textures.push_back(student);
         Textures.push_back(wall);
+        Textures.push_back(wall2);
     }
     void EventsMiniGame(sf::RenderWindow & window){
         for (auto event = sf::Event{}; window.pollEvent(event);) {
@@ -51,12 +74,16 @@ public:
                 window.close();
             }
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {Textures[0].move(0, -SPEED); }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {Textures[0].move(-SPEED, 0); }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {Textures[0].move(0, SPEED); }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {Textures[0].move(SPEED, 0); }
+        
+        SPEED_W = 5, SPEED_A = 5, SPEED_S = 5, SPEED_D = 5;
+        dw = 0, da = 0, ds = 0, dd = 0;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {Textures[0].move(0, -SPEED_W); dw = 1; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {Textures[0].move(-SPEED_A, 0); da = 1; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {Textures[0].move(0, SPEED_S); ds = 1; }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {Textures[0].move(SPEED_D, 0); dd = 1; }
 
-
+        Collision(Textures[1]);
+        Collision(Textures[2]);
     }
 };
 
