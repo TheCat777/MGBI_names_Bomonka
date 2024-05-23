8566cd0ea61dc6f26c729dcc79fc17fac3c19a6f
Yaroslavver <103107983+Yaroslavver@users.noreply.github.com>
2024-05-23 23:39:58 +0300

    Добавил больше функций в диалоги.
    Теперь можно больше контролировать игру через файл. Текст, фоны, музыка, имена и многое другое. Улучшилась структура файла с сюжетом. Изменился Парсер

--- Game/Parser.h
+++ Game/Parser.h
@@ -13,8 +13,9 @@ struct nowScene
 {
     int id;
     std::wstring text;
-    std::string prepod;
-    std::string fon;
+    std::wstring name;
+    int prepod;
+    int fon;
     std::string music;
     int button_count;
     std::vector <std::pair<std::wstring, int>> buttons;
@@ -31,27 +32,47 @@ public:
             std::cerr << "Error open parser file!" << std::endl;
         }
         std::string line;
+        int numlines = 0;
         while (std::getline(file, line)) {
             std::istringstream iss(line);
 
             nowScene newline;
 
+            // (id)   SYS   FON   PREPOD   Music   BUT-Count   NAME   TEXT   BUTTONS
+            
             //iss >> newline.id;
             std::string time_id;
-            std::getline(iss, time_id, '\t');
-            newline.id = std::stoi(time_id);
+            //std::getline(iss, time_id, '\t');
+            newline.id = numlines;//std::stoi(time_id);
+            numlines++;
 
-            std::string text;
-            std::getline(iss, text, '\t');
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-            newline.text = converter.from_bytes(text);
+            std::getline(iss, newline.sys, '\t');
             
-            std::getline(iss, newline.prepod, '\t');
-            std::getline(iss, newline.fon, '\t');
+            std::string time_fon;
+            std::getline(iss, time_fon, '\t');
+            newline.fon = std::stoi(time_fon);
+
+            std::string time_prepod;
+            std::getline(iss, time_prepod, '\t');
+            newline.prepod = std::stoi(time_prepod);
+
             std::getline(iss, newline.music, '\t');
+
             std::string time_button_count;
             std::getline(iss, time_button_count, '\t');
             newline.button_count = std::stoi(time_button_count);
+
+            std::string name;
+            std::getline(iss, name, '\t');
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+            if(name != ""){
+                newline.name = converter.from_bytes(name);
+            }
+            else if(newline.id > 0){newline.name = SceneMap[newline.id-1].name;}
+            
+            std::string text;
+            std::getline(iss, text, '\t');
+            newline.text = converter.from_bytes(text);
 
             for(int i = 0; i < newline.button_count; i++){
                 std::string convert_text;
@@ -65,24 +86,6 @@ public:
                 button_info.second = std::stoi(time_buttons);
                 newline.buttons.push_back(button_info);
             }
-            std::getline(iss, newline.sys, '\t');
-            
-             
-            /*
-            nowScene newLine;
-            std::string elem;
-            int numelem = 0;
-            while (iss >> elem) {
-                switch(numelem){
-                    case 0: newLine.id = std::stoi(elem);
-                    case 1: newLine.id = std::stoi(elem);
-                    case 0: newLine.id = std::stoi(elem);
-                    case 0: newLine.id = std::stoi(elem);
-                    case 0: newLine.id = std::stoi(elem);
-                    case 0: newLine.id = std::stoi(elem);
-                }
-            }
-            */
 
             SceneMap[newline.id] = newline;
         }
